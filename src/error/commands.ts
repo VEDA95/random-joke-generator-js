f0c2c52c07a8fbc8161941f20dc9061a3de51525
Stefan Netterfield <stefan@5f03.com>
2023-06-21 04:28:58 -0400

    Adding error handlers to error module for commands module...

---
+++ src/error/commands.ts
@@ -0,0 +1,4 @@
+import {throwGenericError, throwTypeError, throwRangeError} from './throw';
+
+export function raiseTypeError(field: string, providedValue: any, correctValue: any, err: any): void {
+}
