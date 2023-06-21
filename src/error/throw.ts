120c693619fce58c692c2c51e86c68c2d97baea7
Stefan Netterfield <stefan@5f03.com>
2023-06-21 04:16:27 -0400

    Implemented functions for type errors and range errors...

--- src/error/throw.ts
+++ src/error/throw.ts
@@ -1,11 +1,21 @@
 
-export function throwGenericError(errMessage: string, traceLimit: number = 0, cause?: Error): void {
+export function throwGenericError(errMessage: string, traceLimit: number = 0, cause?: any): void {
     Error.stackTraceLimit = traceLimit;
     const err: Error = cause != null ? new Error(errMessage, {cause}) : new Error(errMessage);
 
     throw err;
 }
 
-export function throwTypeError(): void {}
+export function throwTypeError(errMessage: string, traceLimit: number = 0, cause?: any): void {
+    Error.stackTraceLimit = traceLimit;
+    const err: Error = cause != null ? new TypeError(errMessage, {cause}) : new TypeError(errMessage);
 
-export function throwRangeError(): void {}
+    throw err;
+}
+
+export function throwRangeError(errMessage: string, traceLimit: number = 0, cause?: any): void {
+    Error.stackTraceLimit = traceLimit;
+    const err: Error = cause != null ? new RangeError(errMessage, {cause}) : new RangeError(errMessage);
+
+    throw err;
+}
