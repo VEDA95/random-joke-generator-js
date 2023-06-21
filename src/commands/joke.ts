bec7fb93ac1edc958875887cae1fe90062315e0a
Stefan Netterfield <stefan@5f03.com>
2023-06-21 19:04:36 -0400

    Refactored error joke error handling to resolve bugs occuring during validation...

--- src/commands/joke.ts
+++ src/commands/joke.ts
@@ -2,56 +2,56 @@ import commander from 'commander';
 import {raiseValueError, raiseTypeError, raiseValueRangeError} from '../error/commands';
 import type {Command} from 'commander';
 
+interface IJokeOptions {
+    [index: string]: any;
+}
+
 const joke: Command = new commander.Command('joke');
 const jokeCommand: Command = joke
     .description('Command for telling jokes.')
     .option('-c, --category [cat]', 'Select preferred category for the joke you are being told... (options: any, programming, misc, dark, pun, spooky, christmas)', 'any')
     .option('-l, --language [lang]', 'Select the language you would like the joke to be told in. (options: english, czech, french, german, portuguese, spanish)', 'english')
-    .option('-f --flag [fl...]', 'Select which types of jokes you would like to filter out (options: nsfw, religious, political, racist, sexist, explicit)')
+    .option('-a --amount [amnt]', 'Select the amount of jokes you would like to be told (limit: 10)', '1')
     .option('-t --type [valueType]', 'Select how many parts the joke should be. (options: single, two-part, both)', 'both')
+    .option('-f --flag [fl...]', 'Select which types of jokes you would like to filter out (options: nsfw, religious, political, racist, sexist, explicit)')
     .option('-s --search [query]', 'Search for jokes that contains a search term.')
     .option('-i --id [jokeIds...]', 'Search for jokes by a range of id numbers. (or one if that floats your boat...)')
-    .option('-a --amount [amnt]', 'Select the amount of jokes you would like to be told (limit: 120)', '1')
-    .action((cat, lang, fl, valueType, query, ids, amnt): void => {
+    .action((options: IJokeOptions): void => {
+        const {category, language, amount, type, flag, id}: IJokeOptions = options;
         const validCatValues: Array<string> = ['any', 'programming', 'misc', 'dark', 'pun', 'spooky', 'christmas'];
         const validLangValues: Array<string> = ['english', 'czech', 'french', 'german', 'portuguese', 'spanish'];
         const validFlagValues: Array<string> = ['nsfw', 'religious', 'political', 'racist', 'sexist', 'explicit'];
         const validValueTypeValues: Array<string> = ['single', 'two-part', 'both'];
-        const catType: string = typeof cat;
-        const langType: string = typeof lang;
-        const valType: string = typeof valueType;
-        const queryType: string = typeof query;
-
+        const catType: string = typeof category;
+        const langType: string = typeof language;
+        const valueTypeValue: string = typeof type;
 
         if(catType !== 'string') raiseTypeError('category', catType, 'string');
-        if(!validCatValues.includes(cat)) raiseValueError('category', cat, validCatValues);
+        if(!validCatValues.includes(category)) raiseValueError('category', category, validCatValues);
         if(langType !== 'string') raiseTypeError('language', langType, 'string');
-        if(!validCatValues.includes(cat)) raiseValueError('language', lang, validLangValues);
-        if(fl != null || fl.length > 0) {
-            const flagArray = fl as Array<any>;
-            let invalidValues: Array<string> = [];
-            const areFlagValuesValid: boolean = flagArray.every((item: any) => validFlagValues.includes(item));
+        if(!validLangValues.includes(language)) raiseValueError('language', language, validLangValues);
+        if(Array.isArray(flag)) {
+            let invalidFlagsTypes: Array<string> = [];
+            const flagArray = flag as Array<string>;
             const areFlagValuesCorrectlyTyped: boolean = flagArray.every((item: any) => {
                 const itemType: string = typeof item;
-                const isString: boolean = itemType === 'string';
+                const isItemString: boolean = itemType === 'string';
 
-                if(!isString) invalidValues = [...invalidValues, itemType];
+                if(!isItemString) invalidFlagsTypes = [...invalidFlagsTypes, itemType];
 
-                return isString;
+                return isItemString;
             });
+            const areFlagValuesValid: boolean = flagArray.every((item: any) => validFlagValues.includes(item));
 
-            if(!areFlagValuesCorrectlyTyped) raiseTypeError('flags', invalidValues.join(' | '), 'Array<string>');
-            if(!areFlagValuesValid) raiseValueRangeError('flags', fl, validFlagValues);
+            if(!areFlagValuesCorrectlyTyped) raiseTypeError('flag', `Array<${invalidFlagsTypes.join(' | ')}>`, 'Array<string>');
+            if(!areFlagValuesValid) raiseValueRangeError('flag', flag, validFlagValues);
         }
-        if(valType !== 'string') raiseTypeError('type', valType, 'string');
-        if(!validValueTypeValues.includes(valueType)) raiseValueError('type', valueType, validValueTypeValues);
-        if(query != null && queryType !== 'string') raiseTypeError('search', queryType, 'string');
-        if(ids != null || ids.length > 0) {
+        if(Array.isArray(id)) {
             let invalidIdTypes: Array<string> = [];
-            const idsArray = ids as Array<string>;
-            const areIdsCorrectlyTyped: boolean = idsArray.every((item: any) => {
+            const idArray = id as Array<string>;
+            const areIdValuesCorrectlyTyped: boolean = idArray.every((item: any) => {
                 try {
-                    parseInt(item);
+                    parseInt(item)
                     return true;
                 } catch {
                     invalidIdTypes = [...invalidIdTypes, typeof item];
@@ -59,15 +59,18 @@ const jokeCommand: Command = joke
                 }
             });
 
-            if(!areIdsCorrectlyTyped) raiseTypeError('ids', invalidIdTypes.join(' | '), 'Array<number>');
+            if(!areIdValuesCorrectlyTyped) raiseTypeError('flag', `Array<${invalidIdTypes.join(' | ')}>`, 'Array<number>');
         }
-        if(amnt != null) {
-            try {
-                parseInt(amnt);
+        if(valueTypeValue !== 'string') raiseTypeError('language', valueTypeValue, 'string');
+        if(!validValueTypeValues.includes(type)) raiseValueError('type', type, validValueTypeValues);
 
-            } catch {
-                raiseTypeError('amount', typeof amnt, 'number');
-            }
+        try {
+            const amountNum: number = parseInt(amount);
+
+            if(amountNum > 10) raiseValueError('amount', amountNum, ['less than or equal to 10']);
+
+        } catch {
+            raiseTypeError('amount', typeof amount, 'number');
         }
 
     });
