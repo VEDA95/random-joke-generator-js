b4e3609124dd2f00ed11db3060b7c4ed0d50d58f
Stefan Netterfield <stefan@5f03.com>
2023-06-23 14:00:30 -0400

    Added command for modifying cache max age...

--- src/commands/cache.ts
+++ src/commands/cache.ts
@@ -5,6 +5,7 @@ const cache: Command = new commander.Command('cache');
 const cacheCommand: Command = cache
         .description('Controls mechanism for caching jokes.')
         .command('enable', 'Enables caching if disabled.')
-        .command('disable', 'Disables caching is enabled.');
+        .command('disable', 'Disables caching is enabled.')
+        .command('maxAge <age>', 'Set the max age for data to be cached.')
 
 export default cacheCommand;
