60afcfeb32423b94956245d8f4c3f96eae63e057
Stefan Netterfield <stefan@5f03.com>
2023-06-23 15:54:05 -0400

    Refactored code to resolve bugs that would result from how random numbers were generated and applied...

--- src/data/random.ts
+++ src/data/random.ts
@@ -1,11 +1,4 @@
 
 export function getRandomId(): number {
-    return Math.random() * 1368;
-}
-
-export function getRandomCategory(): string {
-    const categoryArr: Array<string> = ['Programming', 'Misc', 'Dark', 'Pun', 'Spooky', 'Christmas'];
-    const randomIndex: number = Math.random() * (categoryArr.length - 1);
-
-    return categoryArr[randomIndex];
+    return Math.floor(Math.random() * 319);
 }
